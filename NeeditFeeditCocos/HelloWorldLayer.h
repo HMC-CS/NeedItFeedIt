a96bde6b7cde3ffc4cef90e6897335939aba2a08
haleyerickson <herickson@g.hmc.edu>
2013-06-07 15:57:34 -0700

    Initial Commit

---
+++ NeeditFeeditCocos/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  NeeditFeeditCocos
+//
+//  Created by Haley Erickson on 6/7/13.
+//  Copyright Haley Erickson 2013. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
