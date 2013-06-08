6d9e4ff7ba2785c1fde4a7457ab2442c980bfa9b
p-garratt <pgarratt@hmc.edu>
2013-06-08 13:35:11 -0700

    Added more classes
    Most objects now appear, but we need them to appear not hardcoded

---
+++ NeeditFeeditCocos/Organism.h
@@ -0,0 +1,21 @@
+//
+//  Organism.h
+//  NeeditFeeditCocos
+//
+//  Created by Paige Garratt on 6/8/13.
+//  Copyright 2013 Haley Erickson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface Organism : CCLayer {
+    
+}
+
+-(id) init;
+-(BOOL) isSatisfied;
+-(BOOL) isDead;
+-(void) updateHeath;
+
+@end
---
+++ NeeditFeeditCocos/Resource.m
@@ -0,0 +1,25 @@
+//
+//  Resource.m
+//  NeeditFeeditCocos
+//
+//  Created by Paige Garratt on 6/8/13.
+//  Copyright 2013 Haley Erickson. All rights reserved.
+//
+
+#import "Resource.h"
+
+
+@implementation Resource
+
+-(id) init{
+    if (self = [super init]) {
+        return self;
+    }
+    return nil;
+}
+
+-(void) ccTouchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    
+}
+
+@end
