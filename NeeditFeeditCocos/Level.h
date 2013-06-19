7d94f51293343568b092b8fcf67840379168d419
haleyerickson <herickson@g.hmc.edu>
2013-06-19 15:04:23 -0700

    added files

---
+++ NeeditFeeditCocos/Level.h
@@ -0,0 +1,13 @@
+//
+//  Level.h
+//  NeeditFeeditCocos
+//
+//  Created by Haley Erickson on 6/19/13.
+//  Copyright (c) 2013 Haley Erickson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Level : NSObject
+
+@end
---
+++ NeeditFeeditCocos/Level.m
@@ -0,0 +1,13 @@
+//
+//  Level.m
+//  NeeditFeeditCocos
+//
+//  Created by Haley Erickson on 6/19/13.
+//  Copyright (c) 2013 Haley Erickson. All rights reserved.
+//
+
+#import "Level.h"
+
+@implementation Level
+
+@end
