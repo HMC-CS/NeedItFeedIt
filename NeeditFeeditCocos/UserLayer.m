918b5867a66a18f41ab1fc24b8acc3622d9dcfef
haleyerickson <herickson@g.hmc.edu>
2013-07-01 14:43:33 -0700

    outlined text

--- NeeditFeeditCocos/UserLayer.m
+++ NeeditFeeditCocos/UserLayer.m
@@ -12,9 +12,12 @@
 static const int HEIGHTSCALE = 0.97;
 
 @implementation UserLayer{
-    CCLabelTTF* timeLabel;
-    CCLabelTTF* scoreLabel;
-    CCLabelTTF* multiLabel;
+    CCSprite* timeLabel;
+    CCLabelTTF* timeText;
+    CCSprite* scoreLabel;
+    CCLabelTTF* scoreText;
+    CCSprite* multiLabel;
+    CCLabelTTF* multiText;
     int endValue;
     double delta;
 }
@@ -23,34 +26,50 @@ static const int HEIGHTSCALE = 0.97;
     if (self = [super init]) {
         
         CGSize size = [[CCDirector sharedDirector] winSize];
-        
-        //Create a label
-        timeLabel = [CCLabelTTF labelWithString:@"" fontName:@"Marker Felt" fontSize:36];
+
+
+        //Create timer label
+        timeLabel = [CCSprite spriteWithFile:@"timer.png"];
         timeLabel.position = ccp(5*size.width/6, size.height*HEIGHTSCALE);
-        timeLabel.color = ccc3(68, 14, 98);
         
-        scoreLabel = [CCLabelTTF labelWithString:@"" fontName:@"Marker Felt" fontSize:36];
-        scoreLabel.position = ccp(3*size.width/12, size.height*HEIGHTSCALE);
-        scoreLabel.color = ccc3(68, 14, 98);
+        timeText = [CCLabelTTF labelWithString:@"" fontName:@"Hobo" fontSize:36];
+        timeText.position = ccp(5*size.width/6 + timeLabel.contentSize.width + 10, size.height*HEIGHTSCALE);
+        timeText.color = ccc3(68, 14, 98);
         
-        multiLabel = [CCLabelTTF labelWithString:@"" fontName:@"Marker Felt" fontSize:36];
+        //Create score label
+        scoreLabel = [CCSprite spriteWithFile:@"score.png"];
+        scoreLabel.position = ccp(1*size.width/5, size.height*HEIGHTSCALE);
+        
+        scoreText = [CCLabelTTF labelWithString:@"" fontName:@"Hobo" fontSize:36];
+        scoreText.position = ccp(1*size.width/5 + scoreLabel.contentSize.width/2 + 10, size.height*HEIGHTSCALE);
+        scoreText.color = ccc3(68, 14, 98);
+        
+        //Create multiplier label
+        multiLabel = [CCSprite spriteWithFile:@"multiplier.png"];
         multiLabel.position = ccp(3*size.width/6, size.height*HEIGHTSCALE);
-        multiLabel.color = ccc3(68, 14, 98);;
+        
+        multiText = [CCLabelTTF labelWithString:@"" fontName:@"Hobo" fontSize:36];
+        multiText.position = ccp(3*size.width/6 + multiLabel.contentSize.width/2 + 15, size.height*HEIGHTSCALE);
+        multiText.color = ccc3(68, 14, 98);
         
         _multiplier = 1;
-        multiLabel.string = [NSString stringWithFormat:@"Multiplier: x%d", _multiplier];
+        multiText.string = [NSString stringWithFormat:@" x%d", _multiplier];
         
-        //Add pause button in
+        //Add pause button
         CCMenuItemImage* pause  = [CCMenuItemImage itemWithNormalImage:@"pause.png" selectedImage:@"pausesel.png" target:self selector:@selector(pausePressed:)];
         CCMenu* pauseMenu = [CCMenu menuWithItems:pause, nil];
-        pauseMenu.position = ccp(size.width/8, size.height*HEIGHTSCALE);
+        pauseMenu.position = ccp(size.width/11, size.height*HEIGHTSCALE);
                
         [self updateTimer:0];
         [self updatePoints:0];
         [self addChild: timeLabel];
+        [self addChild: timeText];
         [self addChild: scoreLabel];
+        [self addChild: scoreText];
         [self addChild: multiLabel];
+        [self addChild: multiText];
         [self addChild: pauseMenu];
+        
         
         self.touchEnabled = YES;
         
@@ -60,7 +79,7 @@ static const int HEIGHTSCALE = 0.97;
 }
 
 -(void) updateTimer: (int) seconds{
-    timeLabel.string = [NSString stringWithFormat:@"Timer %02.f : %02i", round(seconds/60), seconds % 60];
+    timeText.string = [NSString stringWithFormat:@" %02.f : %02i", round(seconds/60), seconds % 60];
 }
 
 -(void)setPoints:(int)points
@@ -69,7 +88,7 @@ static const int HEIGHTSCALE = 0.97;
 }
 
 -(void) updatePoints: (int) points{
-    scoreLabel.string = [NSString stringWithFormat:@"Score: %d", points];
+    scoreText.string = [NSString stringWithFormat:@" %d", points];
 }
 
 -(void) pausePressed: (id) sender{
@@ -78,7 +97,7 @@ static const int HEIGHTSCALE = 0.97;
 
 -(void) updateMultiplier:(int)newMulti{
     _multiplier = newMulti;
-    multiLabel.string = [NSString stringWithFormat:@"Multiplier: x%d", _multiplier];
+    multiText.string = [NSString stringWithFormat:@" x%d", _multiplier];
 }
 
 @end
